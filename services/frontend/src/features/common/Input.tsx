7630390858cb755cbeebf1dbfb516e281f618a98
riflowth <riflowth@gmail.com>
2022-10-31 13:31:59 +0700

    refactor: input component more flexible style

--- services/frontend/src/features/common/Input.tsx
+++ services/frontend/src/features/common/Input.tsx
@@ -31,7 +31,7 @@ export const Input = ({
       {label && <label className="block mb-2 text-neutral-500">{label}</label>}
       <Row
         center="secondary"
-        className="w-full text-black dark:text-white dark:bg-black border border-neutral-200 dark:border-neutral-600 rounded-md focus-within:outline transition-colors ease-in duration-200"
+        className="w-full text-black dark:text-white dark:bg-black border border-neutral-200 dark:border-neutral-600 rounded-md focus-within:border focus-within:border-black dark:focus-within:border-neutral-200 transition-colors ease-in duration-200"
       >
         {icon && (
           <div className="pl-3">
@@ -40,7 +40,7 @@ export const Input = ({
         )}
         <input
           type={type} 
-          className="w-full px-3 py-2 bg-transparent focus:outline-none rounded-md transition-colors ease-in duration-200"
+          className="w-full px-3 py-2 bg-transparent focus:outline-none"
           placeholder={placeholder}
         />
       </Row>
