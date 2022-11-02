6ed4288a53d32106252f39cf75073a0060449ba0
riflowth <riflowth@gmail.com>
2022-11-02 14:57:51 +0700

    feat: can back to dashboard with logo

--- services/frontend/src/features/common/CodernLogo.tsx
+++ services/frontend/src/features/common/CodernLogo.tsx
@@ -1,6 +1,7 @@
 import { Text } from '@/features/common/Text';
 import { useTheme } from '@/store/ThemeStore';
 import { classNames } from '@/utils/Classes';
+import { route } from 'preact-router';
 
 type CodernLogoProps = {
   className?: string,
@@ -12,7 +13,10 @@ export const CodernLogo = ({
   const [theme, setTheme, selectedTheme] = useTheme();
 
   return (
-    <span className={classNames('flex flex-row items-center', className)}>
+    <span
+      className={classNames('flex flex-row items-center hover:cursor-pointer', className)}
+      onClick={() => route('/dashboard')}
+    >
       <img src={`/codern-${selectedTheme}.svg`} alt="Codern Logo" className="w-9 h-9"/>
       <Text className="text-lg font-bold">Codern</Text>
     </span>
