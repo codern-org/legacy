78f74f7057f03ca326ecc6647a6624eaa52cb4be
riflowth <riflowth@gmail.com>
2022-11-03 01:00:25 +0700

    refactor: improve ux/ui on editor related

--- services/frontend/src/features/question/EditorPane.tsx
+++ services/frontend/src/features/question/EditorPane.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/features/common/Button';
 import { Editor } from '@/features/question/Editor';
 import { editorCodeAtom, editorSettingsAtom, isSupportedEditorLanguage } from '@/store/EditorStore';
 import { defaultCodeByLanguage } from '@/store/mockup/EditorMockup';
-import { TrashIcon } from '@heroicons/react/24/outline';
+import { ArrowPathIcon, Cog6ToothIcon } from '@heroicons/react/24/outline';
 import { useAtom } from 'jotai';
 
 export const EditorPane = () => {
@@ -23,20 +23,19 @@ export const EditorPane = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="h-full flex rounded-lg overflow-hidden">
+      <div className="h-full flex rounded-lg overflow-hidden border border-primary">
         <Editor />
       </div>
 
       <div className="flex flex-row justify-between space-x-2 mt-6">
         <div className="flex flex-row space-x-2">
-          <select
-            className="w-32 pl-4 pr-8 text-sm text-black dark:text-white dark:bg-black border border-primary hover:border-primary-hover rounded-lg focus:outline-none"
-            onInput={handleLanguageChange}
+          {/* TODO: setting */}
+          <Button
+            color="secondary"
+            className="h-full"
           >
-            <option value="c">C</option>
-            <option value="cpp">C++</option>
-            <option value="javascript">JavaScript (For testing only)</option>
-          </select>
+            <Cog6ToothIcon className="w-5 h-5" />
+          </Button>
 
           <div className="relative group">
             <span className="hidden group-hover:block absolute left-1/2 bottom-0 w-fit px-2 py-1 text-sm text-neutral-300 whitespace-nowrap rounded-lg bg-neutral-700 transform -translate-y-12 -translate-x-1/2">
@@ -47,9 +46,18 @@ export const EditorPane = () => {
               className="h-full"
               onClick={handleResetCode}
             >
-              <TrashIcon className="w-5 h-5" />
+              <ArrowPathIcon className="w-5 h-5" />
             </Button>
           </div>
+
+          <select
+            className="w-32 pl-4 pr-8 text-sm text-black dark:text-white dark:bg-black border border-primary hover:border-primary-hover rounded-lg focus:outline-none"
+            onInput={handleLanguageChange}
+          >
+            <option value="c">C</option>
+            <option value="cpp">C++</option>
+            <option value="javascript">JavaScript (For testing only)</option>
+          </select>
         </div>
         <Button className="px-8">Submit</Button>
       </div>
