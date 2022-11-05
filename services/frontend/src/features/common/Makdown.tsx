6a5a237ca4e9fd85df3c1572b5594d60e242b936
riflowth <riflowth@gmail.com>
2022-11-05 17:44:45 +0700

    fix: border color not same as web theme

--- services/frontend/src/features/common/Makdown.tsx
+++ services/frontend/src/features/common/Makdown.tsx
@@ -17,7 +17,12 @@ export const Markdown = ({
       className={classNames(
         'prose dark:prose-invert',
         'prose-blockquote:border-neutral-300 dark:prose-blockquote:border-neutral-700',
-        'prose-li:marker:text-neutral-400 dark:prose-li:marker:text-neutral-600'
+        'prose-li:marker:text-neutral-400 dark:prose-li:marker:text-neutral-600',
+
+        'prose-th:p-2 prose-td:p-2',
+        'prose-table:border-neutral-300 dark:prose-table:border-neutral-700',
+        'prose-thead:border-neutral-300 dark:prose-thead:border-neutral-700',
+        'prose-tr:border-neutral-300 dark:prose-tr:border-neutral-700',
       )}
     />
   );
