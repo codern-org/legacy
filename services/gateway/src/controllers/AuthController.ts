96034020d14057d7fb52a13b4e753db02b330cdc
riflowth <riflowth@gmail.com>
2022-11-13 20:32:38 +0700

    feat: add working logout button

--- services/gateway/src/controllers/AuthController.ts
+++ services/gateway/src/controllers/AuthController.ts
@@ -1,10 +1,10 @@
 import {
-  Controller, Get, Headers, Inject, Ip, Query, Redirect, Res,
+  Controller, Get, Headers, Inject, Ip, Query, Redirect, Req, Res,
 } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { firstValueFrom, Observable } from 'rxjs';
 import { GoogleAuthUrlResponse } from 'api-types';
-import { FastifyReply } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { ConfigService } from '@nestjs/config';
 import { AuthService } from '@/services/AuthService';
 
@@ -20,6 +20,19 @@ export class AuthController {
   ) {
     this.client = client;
     this.configService = configService;
+  }
+
+  @Get('/logout')
+  public async logout(
+    @Req() request: FastifyRequest,
+    @Res({ passthrough: true }) response: FastifyReply,
+  ): Promise<void> {
+    const session = request.cookies.sid;
+    if (!session) return;
+
+    const service = this.client.getService<AuthService>('AuthService');
+    await firstValueFrom(service.logout({ session }));
+    response.header('Set-Cookie', 'sid=; path=/ expires=Thu, 01 Jan 1970 00:00:00 GMT');
   }
 
   @Get('/google')
