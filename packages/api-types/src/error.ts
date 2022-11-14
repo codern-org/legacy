4179c5b40209a943575659d914e038ac8a88da46
riflowth <riflowth@gmail.com>
2022-11-14 19:52:21 +0700

    refactor: add related grpc and error types

--- packages/api-types/src/error.ts
+++ packages/api-types/src/error.ts
@@ -1,18 +1,28 @@
-export enum GrpcError {
-  CANCELLED = 1,
-  UNKNOWN,
-  INVALID_ARGUMENT,
-  DEADLINE_EXCEEDED,
-  NOT_FOUND,
-  ALREADY_EXISTS,
-  PERMISSION_DENIED,
-  RESOURCE_EXHAUSTED,
-  FAILED_PRECONDITION,
-  ABORTED,
-  OUT_OF_RANGE,
-  UNIMPLEMENTED,
-  INTERNAL,
-  UNAVAILABLE,
-  DATA_LOSS,
-  UNAUTHENTICATED,
+/* eslint-disable max-classes-per-file */
+
+export type ErrorDetail = {
+  code: string,
+  error: string,
+  message: string,
+};
+
+export class ExpectedError extends Error {
+
+  public code: string;
+  public error: string;
+  public message: string;
+
+  public constructor(detail: ErrorDetail) {
+    super();
+    this.code = detail.code;
+    this.error = detail.error;
+    this.message = detail.message;
+  }
+
 }
+
+export class NotFoundExpectedError extends ExpectedError {}
+
+export class InvalidExpectedError extends ExpectedError {}
+
+export class DuplicatedExpectedError extends ExpectedError {}
