445449a81aa6450a995a8d1253b91e05664d08c8
riflowth <riflowth@gmail.com>
2022-11-20 23:04:44 +0700

    feat: add grading controller

--- services/gateway/src/App.ts
+++ services/gateway/src/App.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { LoggerConfig } from 'logger';
 import { WinstonModule, WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import fastifyCookie from '@fastify/cookie';
+import fastifyMultipart from '@fastify/multipart';
 import { AppModule } from '@/modules/AppModule';
+import { AllExceptionFilter } from '@/utils/AllExceptionFilter';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -24,6 +27,10 @@ const bootstrap = async (): Promise<void> => {
   // TODO: change origin later
   app.enableCors({ origin: true, credentials: true });
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new AllExceptionFilter(logger));
+
+  app.register(fastifyMultipart);
   app.register(fastifyCookie);
 
   await app.listen(port);
