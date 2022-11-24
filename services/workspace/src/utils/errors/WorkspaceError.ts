97de3aac3f2327980cf5e2673a258032b90d63fa
PPHamster <shinnapatjr@gmail.com>
2022-11-24 08:09:12 +0700

    refactor: add custom question and workspace error

---
+++ services/workspace/src/utils/errors/WorkspaceError.ts
@@ -0,0 +1,7 @@
+export const WorkspaceError = {
+  NotFoundFromWorkspaceId: {
+    code: 'W-001-001',
+    error: 'Workspace not found',
+    message: 'Cannot browse workspace from this id',
+  },
+};
