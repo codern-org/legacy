5543b3520e22717c5f9562ac648d6fff739ebeae
riflowth <riflowth@gmail.com>
2022-12-19 00:07:07 +0700

    refactor: more url controller for sub services

--- services/gateway/src/modules/SharedModule.ts
+++ services/gateway/src/modules/SharedModule.ts
@@ -1,66 +1,88 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProvider, ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
+        imports: [ConfigModule],
         name: 'AUTH_PACKAGE',
-        transport: Transport.GRPC,
-        options: {
-          url: 'localhost:3001',
-          package: 'codern.auth',
-          protoPath: 'root.proto',
-          loader: {
-            includeDirs: [
-              (process.env.NODE_ENV === 'production')
-                ? join(process.cwd(), 'proto')
-                : join(__dirname, '../../../../packages/proto'),
-            ],
-            keepCase: true,
-            arrays: true,
-            longs: Number,
-          },
+        useFactory: async (configService: ConfigService): Promise<ClientProvider> => {
+          const url = configService.get('serviceAuthUrl');
+          return {
+            transport: Transport.GRPC,
+            options: {
+              url,
+              package: 'codern.auth',
+              protoPath: 'root.proto',
+              loader: {
+                includeDirs: [
+                  (process.env.NODE_ENV === 'production')
+                    ? join(process.cwd(), 'proto')
+                    : join(__dirname, '../../../../packages/proto'),
+                ],
+                keepCase: true,
+                arrays: true,
+                longs: Number,
+              },
+            },
+          };
         },
+        inject: [ConfigService],
       },
       {
+        imports: [ConfigModule],
         name: 'WORKSPACE_PACKAGE',
-        transport: Transport.GRPC,
-        options: {
-          url: 'localhost:3002',
-          package: 'codern.workspace',
-          protoPath: 'root.proto',
-          loader: {
-            includeDirs: [
-              (process.env.NODE_ENV === 'production')
-                ? join(process.cwd(), 'proto')
-                : join(__dirname, '../../../../packages/proto'),
-            ],
-            keepCase: true,
-            arrays: true,
-            longs: Number,
-          },
+        useFactory: async (configService: ConfigService): Promise<ClientProvider> => {
+          const url = configService.get('serviceWorkspaceUrl');
+          return {
+            transport: Transport.GRPC,
+            options: {
+              url,
+              package: 'codern.workspace',
+              protoPath: 'root.proto',
+              loader: {
+                includeDirs: [
+                  (process.env.NODE_ENV === 'production')
+                    ? join(process.cwd(), 'proto')
+                    : join(__dirname, '../../../../packages/proto'),
+                ],
+                keepCase: true,
+                arrays: true,
+                longs: Number,
+              },
+            },
+          };
         },
+        inject: [ConfigService],
       },
       {
+        imports: [ConfigModule],
         name: 'GRADING_PACKAGE',
-        transport: Transport.GRPC,
-        options: {
-          url: 'localhost:3003',
-          package: 'codern.grading',
-          protoPath: 'root.proto',
-          loader: {
-            includeDirs: [
-              (process.env.NODE_ENV === 'production')
-                ? join(process.cwd(), 'proto')
-                : join(__dirname, '../../../../packages/proto'),
-            ],
-            keepCase: true,
-            arrays: true,
-            longs: Number,
-          },
+        useFactory: async (configService: ConfigService): Promise<ClientProvider> => {
+          const url = configService.get('serviceGradingUrl');
+          return {
+            transport: Transport.GRPC,
+            options: {
+              url,
+              package: 'codern.grading',
+              protoPath: 'root.proto',
+              loader: {
+                includeDirs: [
+                  (process.env.NODE_ENV === 'production')
+                    ? join(process.cwd(), 'proto')
+                    : join(__dirname, '../../../../packages/proto'),
+                ],
+                keepCase: true,
+                arrays: true,
+                longs: Number,
+              },
+            },
+          };
         },
+        inject: [ConfigService],
       },
     ]),
   ],
@@ -68,4 +90,4 @@ import { join } from 'path';
     ClientsModule,
   ],
 })
-export class SharedModule {}
+export class SharedModule { }
--- services/gateway/src/utils/Configuration.ts
+++ services/gateway/src/utils/Configuration.ts
@@ -1,8 +1,8 @@
 export type Configuration = {
   port: number,
-  serviceAuthPort: number,
-  serviceWorkspacePort: number,
-  serviceGradingPort: number,
+  serviceAuthUrl: string,
+  serviceWorkspaceUrl: string,
+  serviceGradingUrl: string,
   filerUrl: string,
   frontendLoginUrl: string,
   frontendHomeUrl: string,
@@ -10,10 +10,10 @@ export type Configuration = {
 
 export const configuration = (): Configuration => ({
   port: Number.parseInt(process.env.PORT || '3000', 10),
-  serviceAuthPort: Number.parseInt(process.env.SERVICE_AUTH_PORT || '3001', 10),
-  serviceWorkspacePort: Number.parseInt(process.env.SERVICE_WORKSPACE_PORT || '3002', 10),
-  serviceGradingPort: Number.parseInt(process.env.SERVICE_GRADING_PORT || '3003', 10),
-  filerUrl: process.env.FILER_URL || '',
-  frontendLoginUrl: process.env.FRONTEND_LOGIN_URL || '',
-  frontendHomeUrl: process.env.FRONTEND_HOME_URL || '',
+  serviceAuthUrl: process.env.SERVICE_AUTH_URL || 'localhost:3001',
+  serviceWorkspaceUrl: process.env.SERVICE_WORKSPACE_URL || 'localhost:3002',
+  serviceGradingUrl: process.env.SERVICE_GRADING_URL || 'localhost:3003',
+  filerUrl: process.env.FILER_URL || 'http://localhost:8888',
+  frontendLoginUrl: process.env.FRONTEND_LOGIN_URL || 'http://localhost:5555',
+  frontendHomeUrl: process.env.FRONTEND_HOME_URL || 'http://localhost:5555/dashboard',
 });
