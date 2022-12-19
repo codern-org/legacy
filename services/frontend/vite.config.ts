38ff3fc548e7e5073cb2db3196e5f1d343638ab6
riflowth <riflowth@gmail.com>
2022-12-19 14:23:11 +0700

    build: output with `es2022` instead of `cjs`

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import preact from '@preact/preset-vite';
 import path from 'path';
-import commonjs from 'vite-plugin-commonjs';
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -10,7 +9,6 @@ export default ({ mode }) => {
     // https://github.com/vitejs/vite/discussions/8726
     plugins: [
       preact(),
-      commonjs(),
     ],
     resolve: {
       alias: {
@@ -21,13 +19,5 @@ export default ({ mode }) => {
       port: Number.parseInt(process.env.VITE_PORT),
     },
     base: process.env.VITE_BASE_URL,
-    optimizeDeps: {
-      include: ['@codern/external'],
-    },
-    build: {
-      commonjsOptions: {
-        include: [/external/, /node_modules/],
-      },
-    }, 
   });
 };
